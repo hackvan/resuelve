9bcf1d3ef7f684ed9dc0474c12684597980348fc
Diego Camacho <hackvan@gmail.com>
2020-09-30 14:29:20 -0500

    Implement class Level methods

--- lib/classes/level.rb
+++ lib/classes/level.rb
@@ -1,5 +1,6 @@
 class Level
   attr_accessor :name, :goals_by_month
+  @@all = []
 
   def initialize(name:, goals_by_month:)
     @name = name
@@ -7,11 +8,22 @@ class Level
   end
 
   def self.create(name:, goals_by_month:)
+    new(name: name, goals_by_month: goals_by_month).tap { |s| s.save }
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
   end
 
   def self.find_by_name(name)
+    self.all.detect{ |a| a.name == name }
   end
 
   def save
+    @@all << self unless @@all.include?(self)
   end
 end
--- spec/level_spec.rb
+++ spec/level_spec.rb
@@ -1,19 +1,41 @@
 require_relative '../lib/classes/level'
 
 RSpec.describe Level do
+  before(:all) do
+    @level = Level.new(name: 'A', goals_by_month: 10)
+  end
+
   describe 'class methods' do
-    xit '.create' do
+    it '.create' do
+      level = Level.create(name: 'B', goals_by_month: 15)
+      expect(Level.all).to include(level)
     end
 
-    xit '.find_by_name' do
+    describe '.find_by_name' do
+      before(:all) do
+        @level_find = Level.create(name: 'C', goals_by_month: 20)
+      end
+
+      it 'with a valid result' do
+        search = Level.find_by_name('C')
+        expect(search).to eq(@level_find)
+      end
+
+      it 'with a not valid result' do
+        search = Level.find_by_name('X')
+        expect(search).to be_nil
+      end
     end
   end
 
   describe 'instance methods' do
-    xit '#name' do
+    it '#name' do
+      expect(@level.name).to eq('A')
     end
 
-    xit '#save' do
+    it '#save' do
+      @level.save
+      expect(Level.all).to include(@level)
     end
   end
 end
