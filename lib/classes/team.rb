4d06e5242e4be2cfea81d8956a95994dfd558280
Diego Camacho <hackvan@gmail.com>
2020-09-30 15:22:55 -0500

    Implement Team class methods

--- lib/classes/team.rb
+++ lib/classes/team.rb
@@ -1,8 +1,61 @@
 class Team
   attr_accessor :name, :players
+  @@all = []
 
   def initialize(name:)
     @name = name
     @players = []
+  end
+
+  def self.create(name:)
+    new(name: name).tap { |s| s.save }
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def self.print_stats
+    self.all.each do |team|
+      long = 80
+      puts "*" * long
+      puts "Equipo: #{team.name}"
+      puts "Total Goles: #{team.goals_by_month}"
+      puts "Total Minimos: #{team.goals_by_levels}"
+      puts "Alcance: #{'%.2f' % (team.average * 100)}%"
+      puts "Total Jugadores: #{team.players.size}"
+      puts "-" * long
+      team.players.each do |jugador|
+        puts ">> Nombre: #{jugador.name} - Alcance: #{'%.2f' % (jugador.average * 100)}% - Sueldo Completo: #{'%.2f' % jugador.total_payment}"
+      end
+    end
+  end
+
+  def save
+    @@all << self unless @@all.include?(self)
+  end
+
+  def add_player(player)
+    self.players << player unless self.players.include?(player)
+  end
+
+  def goals_by_month
+    self.players
+      .map{ |player| player.goals_by_month }
+      .reduce(0) { |acum, goals| acum + goals }
+  end
+
+  def goals_by_levels
+    self.players
+      .map{ |player| player.goals_by_level }
+      .reduce(:+)
+  end
+
+  def average
+    self.goals_by_month.to_f / self.goals_by_levels.to_f
   end
 end
--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -1,30 +1,61 @@
 require_relative '../lib/classes/team'
 
+PlayerStub = Struct.new(:name, :goals_by_month, :goals_by_level)
+
 RSpec.describe Team do
+  before(:all) do
+    @team = Team.new(name: 'Rojo')
+  end
+
   describe 'class methods' do
-    xit '.create' do
+    it '.create' do
+      team2 = Team.create(name: 'Azul')
+      expect(Team.all).to include(team2)
     end
   end
 
   describe "instance methods" do
-    xit '#name' do
+    it '#name' do
+      expect(@team.name).to eq 'Rojo'
     end
 
-    xit '#save' do
+    it '#save' do
+      @team.save
+      expect(Team.all).to include(@team)
     end
 
-    xit '#add_player' do
+    it '#add_player' do
+      player1 = PlayerStub.new('Pepe', 10, 10)
+      player2 = PlayerStub.new('Pepe', 5, 10)
+      player3 = PlayerStub.new('Pepe', 0, 10)
+      @team.add_player(player1)
+      @team.add_player(player2)
+      @team.add_player(player3)
+      expect(@team.players.size).to eq(3)
     end
   end
 
   describe 'stats' do
-    xit '#goals_by_month' do
+    before(:all) do
+      @player1 = PlayerStub.new('Pepe', 10, 10)
+      @player2 = PlayerStub.new('Pepe', 5, 10)
+      @player3 = PlayerStub.new('Pepe', 0, 10)
+
+      @team.add_player(@player1)
+      @team.add_player(@player2)
+      @team.add_player(@player3)
     end
 
-    xit '#goals_by_levels' do
+    it '#goals_by_month' do
+      expect(@team.goals_by_month).to eq(15)
     end
 
-    xit '#average' do
+    it '#goals_by_levels' do
+      expect(@team.goals_by_levels).to eq(30)
+    end
+
+    it '#average' do
+      expect(@team.average).to eq(0.5)
     end
   end
 end
