a65b1cc77a8ecf0308495f83296e06419fa8e6f7
Diego Camacho <hackvan@gmail.com>
2020-09-30 16:35:23 -0500

    Implement Player class methods

--- lib/classes/player.rb
+++ lib/classes/player.rb
@@ -1,10 +1,83 @@
+BONUS_DISTRIBUTION = {
+  individual: 0.5,
+  team: 0.5
+}.freeze
+
 class Player
+  attr_reader :team
+  attr_accessor :name, :level, :salary, :base_bonus, :goals_by_month
+  @@all = []
+
   def initialize(name:, level: nil, goals_by_month: nil, salary: nil, base_bonus: nil, team: nil)
     @name = name
     @level = level
     @salary = salary
     @base_bonus = base_bonus
     @goals_by_month = goals_by_month
+    self.team = team if team
+  end
+
+  def self.create(name:, level:, goals_by_month:, salary:, base_bonus:, team:)
+    new(
+      name: name,
+      level: level,
+      goals_by_month: goals_by_month,
+      salary: salary,
+      base_bonus: base_bonus,
+      team: team,
+    ).tap { |s| s.save }
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def team=(team)
+    team.add_player(self)
     @team = team
+  end
+
+  def goals_by_level
+    level.goals_by_month
+  end
+
+  def average
+    self.goals_by_month.to_f / self.goals_by_level.to_f
+  end
+
+  def individual_bonus
+    (self.base_bonus * BONUS_DISTRIBUTION[:individual]) * self.average
+  end
+
+  def team_bonus
+    (self.base_bonus * BONUS_DISTRIBUTION[:team]) * self.team.average
+  end
+
+  def total_bonus
+    ((individual_bonus + team_bonus) <= self.base_bonus ? individual_bonus + team_bonus : self.base_bonus).round(2)
+  end
+
+  def total_payment
+    self.salary + self.total_bonus
+  end
+
+  def team_name
+    self.team.name
+  end
+
+  def level_name
+    self.level.name
+  end
+
+  def level_goals
+    self.level.goals_by_month
   end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,40 +1,160 @@
+require_relative '../lib/classes/team'
+require_relative '../lib/classes/level'
 require_relative '../lib/classes/player'
 
 RSpec.describe Player do
+  before(:all) do
+    Level.load_levels(nil)
+    @a_level = Level.find_by_name('A')
+    @b_level = Level.find_by_name('B')
+    @red_team = Team.create(name: 'Rojo')
+    @blue_team = Team.create(name: 'Azul')
+    @green_team = Team.create(name: 'Verde')
+    @purple_team = Team.create(name: 'Morado')
+  end
+
   describe 'class methods' do
-    xit '.create' do
+    it '.create' do
+      player1 = Player.create(
+        name: 'Tito', 
+        level: 'B', 
+        goals_by_month: 5, 
+        salary: 50_000, 
+        base_bonus: 10_000,
+        team: nil
+      )
+
+      expect(Player.all).to include(player1)
     end
   end
 
   describe 'instance methods' do
-    xit '#name' do
+    before(:all) do
+      @player = Player.new(
+        name: 'Pepe', 
+        level: @a_level, 
+        goals_by_month: 10, 
+        salary: 100_000, 
+        base_bonus: 20_000,
+        team: @red_team
+      )
     end
 
-    xit '#save' do
+    it '#name' do
+      expect(@player.name).to eq 'Pepe'
     end
 
-    xit '#team_name' do
+    it '#save' do
+      @player.save
+      expect(Player.all).to include(@player)
     end
 
-    xit '#level_name' do
+    it '#team=' do
+      @player.team = @red_team
+      expect(@player.team).to eq(@red_team)
+      expect(@red_team.players).to include(@player)
     end
 
-    xit '#level_goals' do
+    it '#team_name' do
+      expect(@player.team_name).to eq(@red_team.name)
+    end
+
+    it '#level_name' do
+      expect(@player.level_name).to eq(@a_level.name)
+    end
+
+    it '#level_goals' do
+      expect(@player.level_goals).to eq(@a_level.goals_by_month)
     end
     
   end
 
   describe 'stats' do
-    xit '#goals_by_level' do
+    context "with expected performance" do
+      before(:all) do
+        @player1_stats = Player.create(
+          name: 'Pepe',
+          level: @b_level,
+          goals_by_month: 10,
+          salary: 100_000,
+          base_bonus: 20_000,
+          team: @blue_team
+        )
+      end
+
+      it '#goals_by_level' do
+        expect(@player1_stats.goals_by_level).to eq(10)
+      end
+
+      it '#average' do
+        expect(@player1_stats.average).to eq(1.00)
+      end
+
+      it '#total_bonus' do
+        expect(@player1_stats.total_bonus).to eq(20_000)
+      end
+
+      it '#total_payment' do
+        expect(@player1_stats.total_payment).to eq(120_000)
+      end
     end
 
-    xit '#average' do
+    context "with lower performance" do
+      before(:all) do
+        @player2_stats = Player.create(
+          name: 'Pepe',
+          level: @b_level,
+          goals_by_month: 5,
+          salary: 100_000,
+          base_bonus: 20_000,
+          team: @green_team
+        )
+      end
+
+      it '#goals_by_level' do
+        expect(@player2_stats.goals_by_level).to eq(10)
+      end
+
+      it '#average' do
+        expect(@player2_stats.average).to eq(0.50)
+      end
+
+      it '#total_bonus' do
+        expect(@player2_stats.total_bonus).to eq(10_000)
+      end
+
+      it '#total_payment' do
+        expect(@player2_stats.total_payment).to eq(110_000)
+      end
     end
 
-    xit '#total_bonus' do
-    end
+    context "with higher performance" do
+      before(:all) do
+        @player2_stats = Player.create(
+          name: 'Pepe',
+          level: @b_level,
+          goals_by_month: 15,
+          salary: 100_000,
+          base_bonus: 20_000,
+          team: @purple_team
+        )
+      end
 
-    xit '#total_payment' do
+      it '#goals_by_level' do
+        expect(@player2_stats.goals_by_level).to eq(10)
+      end
+
+      it '#average' do
+        expect(@player2_stats.average).to eq(1.50)
+      end
+
+      it '#total_bonus' do
+        expect(@player2_stats.total_bonus).to eq(20_000)
+      end
+
+      it '#total_payment' do
+        expect(@player2_stats.total_payment).to eq(120_000)
+      end
     end
   end
 end
